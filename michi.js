16118079053dcbf0f444e148ee5464c43c146b08
束野通洋 <52605072+michimichix521@users.noreply.github.com>
2019-08-13 17:22:02 +0900

    Add files via upload

--- michi.js
+++ michi.js
@@ -1,3 +1,14 @@
+window.onload=init_time;
+var id=window.setInterval(update,50);
+
+function init_time(){
+	console.log("1度だけのmichi!");
+}
+
+function update(){
+
+}
+
 function michi(){
 	document.getElementById("michi").innerHTML="Hello michi!";
 }
