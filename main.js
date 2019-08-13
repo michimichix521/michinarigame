31b9098ab74e95abbede80c643a17e8886112e8b
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 22:04:15 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function init_time()
 {
 	var canvas = document.getElementById("canvas").getContext("2d");;
 
-	var player_image = "img/player_attack.png";
+	var player_image = "img/player.png";
 	var fgcolor = "lightgreen";
 	var bgcolor = "black";
 
