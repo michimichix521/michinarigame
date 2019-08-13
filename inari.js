9b98257a563baef88636d82f55960b48a232e72b
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 17:39:02 +0900

    Add files via upload

--- inari.js
+++ inari.js
@@ -1,14 +1,3 @@
-window.onload=init_time_nari;
-var inari_id=window.setInterval(update,50);
-
-function init_time_nari(){
-	console.log("1度だけのnari!");
-}
-
-function update(){
-
-}
-
 function inari(){
 	document.getElementById("michi").innerHTML="Hello michi!";
 }
