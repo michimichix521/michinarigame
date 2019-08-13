283ffa66116d0fb548f1b47cb09a62ff5f59d804
束野通洋 <52605072+michimichix521@users.noreply.github.com>
2019-08-13 17:37:40 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload=init_time;
+var Id=window.setInterval(update,50);
+
+function init_time(){
+	console.log("1度だけのYes!");
+}
+
+function update(){
+
+}
--- michi.js
+++ michi.js
@@ -1,14 +1,3 @@
-window.onload=init_timeMichi;
-var michiId=window.setInterval(update,50);
-
-function init_timeMichi(){
-	console.log("1度だけのmichi!");
-}
-
-function update(){
-
-}
-
 function michi(){
 	document.getElementById("michi").innerHTML="Hello michi!";
 }
