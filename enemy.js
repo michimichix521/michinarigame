5c0aee67a2c5190c26cab528f8e7005c207eaec2
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 23:44:50 +0900

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -8,6 +8,8 @@ class Enemy
 
 		this.readImage(image);
 		this.rd;
+		this.ex;
+		this.ey;
 	}
 
 	readImage(image)
@@ -16,29 +18,29 @@ class Enemy
 	}
 	showImage()
 	{
-		this.canvas.drawImage(this.image,this.x,this.y,50,50);
+		this.canvas.drawImage(this.image,this.x,this.y,50-25,-25);
 	}
 
 	move(px, py)
 	{
 		if(this.y > py){	//up
-			if(this.y-2>=100){
-				this.y-=2;
+			if(this.y-2>=125){
+				this.y-=1;
 			}
 		}
 		if(this.y < py){	//down
-			if(this.y+2<=350){
-				this.y+=2;
+			if(this.y+2<=375){
+				this.y+=1;
 			}	
 		}
 		if(this.x < px){	//right
-			if(this.x+2<=650){
-				this.x+=2;
+			if(this.x+2<=675){
+				this.x+=1;
 			}
 		}
 		if(this.x > px){	//left
-			if(this.x-2>=0){
-				this.x-=2;
+			if(this.x-2>=25){
+				this.x-=1;
 			}
 		}
 /*		this.rd = Math.floor(Math.random() * Math.floor(5));
