d2fe5292a27ecaccd03492e4caf929d375b10303
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 22:28:47 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ window.onload=init_time; //最初にロードされる
 var id=window.setInterval(update,10); //アップロードされる
 
 var player;
-var stage;	
+var stage;
+var enemy;	
 
 function init_time()
 {
 	var canvas = document.getElementById("canvas").getContext("2d");;
 
 	var player_image = "img/player.png";
+	var enemy_image = "img/enemy.png";
 	var fgcolor = "lightgreen";
 	var bgcolor = "black";
 
-	player = new Player(canvas, player_image, 350, 250);
 	stage = new Stage(canvas, 700, 500, fgcolor, bgcolor);
+	enemy = new Enemy(canvas, enemy_image, 150, 250);
+	player = new Player(canvas, player_image, 350, 250);
 	
 }
 
@@ -21,5 +24,7 @@ function update()
 {
 	stage.draw_canvas();
 	player.showImage();
+	enemy.showImage();
+	
 	
 }
