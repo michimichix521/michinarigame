e20073a61494328443c90e4cd96fe04f2c68551b
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 21:06:23 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-window.onload=init_time;
-var Id=window.setInterval(update,50);
+window.onload=init_time; //最初にロードされる
+var id=window.setInterval(update,10); //アップロードされる
 
-function init_time(){
-	console.log("1度だけのYes!");
-}
+class main
+{
+	var player;
 
-function update(){
+	init_time()
+	{
+		var canvas;	
+		var player_image = "img/player_attack.png";
+		
+		canvas=document.getElementById("canvas").getContext("2d");
+		player = new Player(canvas, player_image, 350, 250);
+		canvas.fillStyle="#000000";
+		canvas.fillRect(0,0,700,500);
 
+		
+	}
+
+	update()
+	{
+		canvas();
+		Player.showimage();
+		
+	}
+	
+	canvas()
+	{	
+		canvas.fillStyle="black";
+		canvas.fillRect(0,0,700,500);
+	
+		canvas.fillStyle="lightgreen";
+		canvas.fillRect(0,100,700,300);
+	
+		
+	}
 }
