af4fb7153de2a5c954e869b2101fc4429f73e1ff
inari3 <43071694+narisyo63@users.noreply.github.com>
2019-08-13 22:59:35 +0900

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -1,5 +1,4 @@
-class Enemy
-{
+class Enemy{
 	constructor(canvas,image,x,y){
 		this.canvas=canvas;
 		this.image=new Image();
@@ -7,44 +6,13 @@ class Enemy
 		this.y=y;
 
 		this.readImage(image);
-		var rd;
 	}
 
-	readImage(image)
-	{
+	readImage(image){
 		this.image.src=image;
 	}
-	showImage()
-	{
-		this.canvas.drawImage(this.image,this.x,this.y,50,50);
-	}
 
-	move()
-	{
-		rd = Math.floor(Math.random() * Math.floor(5));
-		switch(rd){
-			case 0:
-				break;
-			case 1:	//up
-				if(ry-2>=100){
-					ry-=2;
-				}
-				break;
-			case 2:	//dowm
-				if(ry+2<=350){
-					ry+=2;
-				}	
-				break;
-			case 3:	//right
-				if(rx+2<=650){
-					rx+=2;
-				}
-				break;
-			case 4:	//left
-				if(rx-2>=0){
-					rx-=2;
-				}
-				break;
-		}
+	showImage(){
+		this.canvas.drawImage(this.image,this.x,this.y,50,50);
 	}
 }
