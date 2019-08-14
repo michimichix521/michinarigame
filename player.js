c96714927ac5968dc6a9117f982b903a119da9e9
michimichix521 <michimichix521@gmail.com>
2019-08-14 17:52:40 +0900

    Update player.js

--- player.js
+++ player.js
@@ -2,6 +2,7 @@ class Player{
 	constructor(canvas,image,x,y){
 		this.canvas=canvas;
 		this.image=new Array(image.length);
+		this.imageChangeNumber=0;
 		this.x=x;
 		this.y=y;
 		this.ArrowUp=false;
@@ -20,7 +21,7 @@ class Player{
 	}
 
 	showImage(){
-		this.canvas.drawImage(this.image[1],this.x-25,this.y-25,50,50);
+		this.canvas.drawImage(this.image[imageChangeNumber],this.x-25,this.y-25,50,50);
 	}
 
 	move(){
