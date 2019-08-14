4b05048377f97f44d9d92fd60bc01eb3ce51edb5
michimichix521 <michimichix521@gmail.com>
2019-08-14 17:29:17 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,12 +14,9 @@ function init_time()
 	var fgcolor = "lightgreen";
 	var bgcolor = "black";
 
-	console.log("michi");
-
 	stage = new Stage(canvas, 700, 500, fgcolor, bgcolor);
 	enemy = new Enemy(canvas, enemy_image, 150, 250);
 	player = new Player(canvas, player_image, 350, 250);
-	console.log("inari");
 
 }
 
